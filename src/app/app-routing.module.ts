f603b68fab44d6017e7f024705e55d593c83d7a6
Dominikuu <alanccl92@hotmail.com>
2021-08-18 19:45:50 +0800

    Bonus
    Signed-off-by: Dominikuu <alanccl92@hotmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-
-import { DashboardComponent } from "./dashboard/dashboard.component";
-import { HeroesComponent } from "./heroes/heroes.component";
 import { HeroDetailComponent } from "./hero-detail/hero-detail.component";
 import { BonusPointComponent } from "./bonus-point/bonus-point.component";
 
 const routes: Routes = [
   { path: "", redirectTo: "/cl-dashboard", pathMatch: "full" },
-  // { path: "dashboard", component: DashboardComponent },
   { path: "detail/:id", component: HeroDetailComponent },
-  // { path: "heroes", component: HeroesComponent },
   { path: "bonusPoint", component: BonusPointComponent },
 
   // lazy loading
--- src/app/bonus-point/bonus-point.component.ts
+++ src/app/bonus-point/bonus-point.component.ts
@@ -1,63 +1,75 @@
-import { Component, OnInit } from '@angular/core';
-import { BsJs1Service } from './bs-js1.service';
-import { of } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { Component, OnInit } from "@angular/core";
+import { BsJs1Service } from "./bs-js1.service";
+import { forkJoin, of, concat } from "rxjs";
+import { tap } from "rxjs/operators";
 
 @Component({
-  selector: 'app-bonus-point',
-  templateUrl: './bonus-point.component.html',
-  styleUrls: ['./bonus-point.component.css']
+  selector: "app-bonus-point",
+  templateUrl: "./bonus-point.component.html",
+  styleUrls: ["./bonus-point.component.css"],
 })
 export class BonusPointComponent implements OnInit {
+  constructor(private bsJs1Service: BsJs1Service) {}
 
-  constructor(
-    private bsJs1Service: BsJs1Service,
-  ) { }
-
-
-
-
-  ngOnInit() {
-
-  }
+  ngOnInit() {}
 
   redirectToKeiPage() {
-    location.href = `${location.protocol}//kei.careline.localhost:${location.port}/bonusPoint`
+    location.href = `${location.protocol}//kei.careline.localhost:${location.port}/bonusPoint`;
   }
 
   redirectToAnnPage() {
-    location.href = `${location.protocol}//ann.careline.localhost:${location.port}/bonusPoint`
+    location.href = `${location.protocol}//ann.careline.localhost:${location.port}/bonusPoint`;
   }
 
   answerCookie1() {
     //ToDo..
+    const cookieName = "Key";
+    const cookieValue = "ck1";
+    const myDate = new Date();
+    myDate.setMonth(myDate.getMonth() + 12);
+    document.cookie =
+      cookieName +
+      "=" +
+      cookieValue +
+      ";expires=" +
+      myDate +
+      ";domain=.careline.host;path=/bonusPoint";
+    // document.cookie = "";
+    console.log(document.cookie);
   }
 
   answerRxjs1() {
-    const first$ = of('first' ,  2000).pipe(tap(() => console.log('first')));
-    const second$ = of('second', 1500).pipe(tap(() => console.log('second')));;
-    const third$ = of('thrid', 800).pipe(tap(() => console.log('thrid')));;
+    const first$ = of("first", 2000).pipe(tap(() => console.log("first")));
+    const second$ = of("second", 1500).pipe(tap(() => console.log("second")));
+    const third$ = of("thrid", 800).pipe(tap(() => console.log("thrid")));
 
     // 考題限制 : 執行順序需為 : first$ => second$ => third$
     // 預期的Console結果 :
     //  first
     //  second
     //  third
-
     // ToDo...
+    concat(first$, second$, third$).subscribe(() => {});
   }
-
 
   answerJs1() {
     let result: string;
-    this.bsJs1Service.getSample()
-      .forEachChilds((child) => {
-        // ToDo : 實作你的解決方案...
-        result = 'ToDo...';
-      });
+    const parentChildMap: { [key: string]: string[] } = {};
+    this.bsJs1Service.getSample().forEachChilds((child) => {
+      // ToDo : 實作你的解決方案...
+      if (!parentChildMap[child.parent.value]) {
+        parentChildMap[child.parent.value] = child.parent["child"].map(
+          ({ value }) => value
+        );
+      }
+    });
+    const res = [];
+    for (const [parent, childs] of Object.entries(parentChildMap)) {
+      res.push(...childs, parent);
+    }
+    result = res.join(" , ");
 
     // 預期alert的結果 => js 1 answer : child_1_1 , child_1_2 , parent_1 , child_2_1 ,  parent_2 ,child_3_1 , child_3_2 , child_3_3 , parent_3
-    alert(`js 1 answer : ${result}`)
+    alert(`js 1 answer : ${result}`);
   }
-
 }
